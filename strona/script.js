98059b49ff7b5f44d86da0b3a5a856c14bb0bd23
Warrgun <131681381+Warrgun@users.noreply.github.com>
2023-12-10 18:26:36 +0100

    Pog

--- strona/script.js
+++ strona/script.js
@@ -1,23 +1,32 @@
 function checkForm(){
-	let imie = document.querySelector('#name').value;
-	let email = document.querySelector('#email').value;
-	let wiek = document.querySelector('#number').value;
-	let error = document.querySelectorAll('.error');
+	const imie = document.querySelector('#name').value;
+	const email = document.querySelector('#email').value;
+	const wiek = document.querySelector('#number').value;
+	const error = document.querySelectorAll('.error');
 	
 	let arr = [imie, email, wiek];
-	let checkWhiteSpace = (s) => s.trim.length === 0;
+	let checkWhiteSpace = (s) => s.trim().length ===0;
 	let empty = (element) => !element || checkWhiteSpace(element);
 	let regex = /\d/g;
 	let emailValid = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-	
-	if(empty(imie) || regex.test(imie)){
-		return error[0].innerHTML = "Wprowadź poprawne dane";
-	}
-	
-	if(empty(email) || !emailValid.test(email)){
-		return error[1].innerHTML = "Wprowadź poprawne dane";
-	}
-	
+	//let ageCalc
+	//auto scroll do bledu
+	//moze obwodki czerwone  na input kiedy jest blad
+	//jesli kazdy input poprawnymozna wyslas do bazy danych
+
+	return arr.map((element, index) => {
+		if(empty(element)){
+			error[index].innerHTML = "Troszke pusto tutaj";
+		}
+		else if(index === 0 && regex.test(element)){
+			error[index].innerHTML = "Wprowadź poprawne dane";
+		}
+		else if(index === 1 && !emailValid.test(element)){
+			error[index].innerHTML = "Czy to jest e-mail ?!";
+		}
+		else{
+			error[index].innerHTML = "";
+		}
+	}); 
 }
 
-console.log(checkForm());
