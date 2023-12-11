5daaf2d1f31fc0d23ca68921397556a570aebdf4
Warrgun <131681381+Warrgun@users.noreply.github.com>
2023-12-11 18:50:43 +0100

    🤓

--- strona/script.js
+++ strona/script.js
@@ -1,32 +1,44 @@
 function checkForm(){
-	const imie = document.querySelector('#name').value;
-	const email = document.querySelector('#email').value;
-	const wiek = document.querySelector('#number').value;
+	const form = document.querySelector('#survey-form');
+	const imie = document.querySelector('#name');
+	const email = document.querySelector('#email');
+	const wiek = document.querySelector('#number');
 	const error = document.querySelectorAll('.error');
 	
 	let arr = [imie, email, wiek];
+	let validate = [false, false, false];
 	let checkWhiteSpace = (s) => s.trim().length ===0;
 	let empty = (element) => !element || checkWhiteSpace(element);
 	let regex = /\d/g;
 	let emailValid = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+	
 	//let ageCalc
-	//auto scroll do bledu
 	//moze obwodki czerwone  na input kiedy jest blad
-	//jesli kazdy input poprawnymozna wyslas do bazy danych
 
-	return arr.map((element, index) => {
-		if(empty(element)){
+	let check = arr.map((element, index) => {
+		if(empty(element.value)){
 			error[index].innerHTML = "Troszke pusto tutaj";
+			element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 		}
-		else if(index === 0 && regex.test(element)){
+		else if(index === 0 && regex.test(element.value)){
 			error[index].innerHTML = "Wprowadź poprawne dane";
+			element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 		}
-		else if(index === 1 && !emailValid.test(element)){
+		else if(index === 1 && !emailValid.test(element.value)){
 			error[index].innerHTML = "Czy to jest e-mail ?!";
+			element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 		}
 		else{
 			error[index].innerHTML = "";
+			validate[index] = true;
 		}
 	}); 
+
+	if(validate.every(current => current === true)){
+		return form.submit();
+	}
+	else{
+		return check;
+	}
 }
 
