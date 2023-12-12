f44ff87e06622d711901a5effc6ac1f77ed97b9f
Fabio <131681381+Warrgun@users.noreply.github.com>
2023-12-12 09:29:57 +0100

    Watch your step

--- strona/script.js
+++ strona/script.js
@@ -13,28 +13,32 @@ function checkForm(){
 	let emailValid = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 	
 	//let ageCalc
-	//moze obwodki czerwone  na input kiedy jest blad
 
 	let check = arr.map((element, index) => {
 		if(empty(element.value)){
 			error[index].innerHTML = "Troszke pusto tutaj";
+			element.style = "border-color: red";
 			element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 		}
 		else if(index === 0 && regex.test(element.value)){
 			error[index].innerHTML = "Wprowadź poprawne dane";
+			element.style = "border-color: red";
 			element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 		}
 		else if(index === 1 && !emailValid.test(element.value)){
 			error[index].innerHTML = "Czy to jest e-mail ?!";
+			element.style = "border-color: red";
 			element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 		}
 		else{
 			error[index].innerHTML = "";
+			element.style = "border-color: #555";
 			validate[index] = true;
 		}
 	}); 
 
 	if(validate.every(current => current === true)){
+		
 		return form.submit();
 	}
 	else{
